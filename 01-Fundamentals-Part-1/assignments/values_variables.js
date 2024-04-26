3b9c81db70f8dfa8eef03779112c0a4a86133e25
idosumit <psumitcoding@gmail.com>
2024-04-26 16:41:07 +0900

    updated: fundamentals part 1 assignment

---
+++ 01-Fundamentals-Part-1/assignments/values_variables.js
@@ -0,0 +1,13 @@
+let country = "Japan";
+let continent = "Asia";
+let population = "idk man";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/* have to link this to an HTML file, I did that with the index.html. Format looks something like:
+...
+<script src = "/path/to/file"></script>
+</body>
+*/
