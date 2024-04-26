8b89348881edd6aabb10a6039bca82004b8b44b9
idosumit <psumitcoding@gmail.com>
2024-04-26 16:59:08 +0900

    update: data type practice

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 
 console.log(40 + 8 + 23 - 10);
@@ -7,3 +8,29 @@ console.log(23);
 
 let firstName = "Kylian";
 console.log(firstName);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+console.log(typeof Jonas);
+
+/* console gives:
+true
+boolean
+boolean
+number
+string
+undefined
+*/
+
+console.log('--------------------');
+// -----------------------
+javascriptIsFun = 'YES!'; // assigning a new value to an existing variable IS possible in JS since it's dynamic
+console.log(typeof javascriptIsFun); // console gives 'string'
+
+console.log('--------------------');
