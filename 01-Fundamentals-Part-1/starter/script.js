35f8c5f066fa4c433826806040eee64de801a2e6
idosumit <psumitcoding@gmail.com>
2024-05-09 09:42:08 +0900

    update: operators (math, assignment, comparison)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -47,7 +47,6 @@ console.log('--------------------');
 
 //An error/bug that exists with typeof
 console.log(typeof null); // we get "object". This makes no sense.
-*/
 
 let age = 30;
 age = 31;
@@ -56,4 +55,36 @@ const birthYear = 1991;
 // birthYear = 1990;
 
 var job = 'programmer';
-job = 'teacher'
+job = 'teacher';
+*/
+
+//Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Sss';
+console.log(firstName + lastName);
+
+// What if I want space between the first & last names?
+console.log(firstName + ' ' + lastName);
+
+//Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // true
+console.log(ageSarah >= 18); // true
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
