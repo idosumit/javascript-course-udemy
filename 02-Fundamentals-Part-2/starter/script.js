2799fc0121c2b295a487094a3a58f34d48debf13
idosumit <psumitcoding@gmail.com>
2024-05-10 15:40:51 +0900

    update: functions calling other functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -53,8 +53,6 @@ console.log(age1, age2);
 
 // Main difference: function declaration can be called before defining that function (i.e., hoisting possible)
 // Main difference: function expression cannot be called before defining it (JUST LIKE IN C) (can't be hoisted)
-*/
-
 
 // --------------------------------- ARROW FUNCTIONS ----------------------------------- //
 // Arrow function
@@ -70,3 +68,20 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 
 console.log(yearsUntilRetirement(1991, 'Charles'));
+
+*/
+
+
+// --------------------------------- FUNCTIONS CALLING OTHER FUNCTIONS ----------------------------------- //
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePiece = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePiece} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
