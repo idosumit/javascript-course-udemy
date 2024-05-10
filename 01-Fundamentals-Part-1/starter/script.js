415357839afc9abd085a14eba328c46dd59d2f32
idosumit <psumitcoding@gmail.com>
2024-05-10 09:43:29 +0900

    update: switch statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -223,7 +223,6 @@ if (favoUrite === 23) { // === this time
 
 // for not equal operators
 if (favoUrite !== 23) console.log('Why not 23?'); // !== is the strict version.
-*/
 
 // --------------------------------- LOGICAL OPERATORS ------------------------------------//
 const hasDriversLicense = true; //A
@@ -239,4 +238,56 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
     console.log(`Sarah is able to drive!`);
 } else {
     console.log(`Someone else should drive!`);
+}
+*/
+
+// --------------------------------- SWITCH STATEMENTS ------------------------------------//
+
+const day = 'friday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    // why? because without the break, the code keeps executing.
+    // so instead of the console printing: "Plan course...\nGo to cod...", it prints them + "Prepare th.."
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+// let's do the exact same but with if/else statements:
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+}
+else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+}
+else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+}
+else if (day === 'friday') {
+    console.log('Record videos');
+}
+else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+}
+else {
+    console.log('Not a valid day');
 }
