0a82c1cedcb3e73d1b7cd17d4f6395d1a6666919
idosumit <psumitcoding@gmail.com>
2024-05-14 17:40:51 +0900

    update: simple debugging intro

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,18 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// ============================================ DEBUGGING ====================================================
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "cels",
+    value: prompt("Degrees celsius: "),
+  };
+
+  console.table(measurement); // gives a nice table with values
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
