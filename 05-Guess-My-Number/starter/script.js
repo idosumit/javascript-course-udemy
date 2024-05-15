773187e6b90ca8c7aec438d806237fd91b72c6c8
idosumit <psumitcoding@gmail.com>
2024-05-15 13:17:20 +0900

    update: absolute basic DOM manipulation demo

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+// Output: Start guessing...
+// Here, the addition of .textContent gave us the text from the HTML snippet "<p class="message">Start guessing...</p>"
+
+// This is called DOM manipulation apparently? The explanation comes later.
