3f8ab504af4f3531f3f3ea5abd79317a7a6c06d6
idosumit <psumitcoding@gmail.com>
2024-05-20 11:08:49 +0900

    update: 3rd project (zeroing scores, hiding dice at the start)

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// selecting elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1'); // another great way to get elements fast
+const diceElement = document.querySelector('.dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
