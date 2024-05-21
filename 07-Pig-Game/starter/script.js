1ffd7603ea0aa069d8881e026e1ca616d637ac03
idosumit <psumitcoding@gmail.com>
2024-05-21 13:39:23 +0900

    update: switching players, holding scores, declaring winner

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -17,31 +17,64 @@ score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
-const scores = [0, 0];
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
+
+const scores = [0, 0]; // for storing scores when player clicks hold (player0 for position 0, player 1 for position 1)
+
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true; // meaning we are currently playing the game
 
 btnRoll.addEventListener('click', function () {
-  // 1. generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // 1. generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. display dice
-  diceElement.classList.remove('hidden');
-  diceElement.src = `dice-${dice}.png`;
+    // 2. display dice
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice-${dice}.png`;
 
-  // 3. check whether the rolled is 1 -> if true, switch to the other player
-  if (dice !== 1) {
-    // add dice to the current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0Element.classList.toggle('player--active');
-    player1Element.classList.toggle('player--active');
-    // toggle method adds the class if it is NOT there and removes the class if it is there. Pretty nice. In this case, toggle basically adds or removes the 'player--active' class thereby switching the background colours.
+    // 3. check whether the rolled is 1 -> if true, switch to the other player
+    if (dice !== 1) {
+      // add dice to the current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+      // Switch to next player
+      // toggle method adds the class if it is NOT there and removes the class if it is there. Pretty nice. In this case, toggle basically adds or removes the 'player--active' class thereby switching the background colours.
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. add current score to the score of the active player
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //2. check if player's score >= 100
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      // Finish the game
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      diceElement.classList.add('hidden');
+    } else {
+      //3. switch the player
+      switchPlayer();
+    }
   }
 });
