564947bc78acd07d9423671f4d5ae70fba9b1272
idosumit <psumitcoding@gmail.com>
2024-05-29 13:02:05 +0900

    update: spread operator and rest pattern

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -137,8 +137,6 @@ console.log(open, close);
 // const [p = 1, q = 1, r = 1] = [8, 9];
 // console.log(p, q, r); // output: 8 9 1
 
-*/
-
 // ============================================ Spread Operator ===================================================
 
 const arr = [7, 8, 9];
@@ -151,6 +149,7 @@ console.log(goodArr); // output: [1, 2, 7, 8, 9]
 // ============== Going more complex
 
 // Example from above:
+*/
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -191,8 +190,15 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your pasta with ${ing1}, ${ing2}, and ${ing3}.`);
   },
+
+  // adding yet another method here for rest pattern
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
+/*
 // what we'll do:
 const newMenu = [...restaurant.mainMenu, 'Gnocci'];
 console.log(newMenu); // Output: ['Pizza', 'Pasta', 'Risotto', 'Gnocci']
@@ -239,3 +245,44 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Krispy Kreme';
 console.log('Copy name: ', restaurantCopy.name);
 console.log('Original name: ', restaurant.name);
+*/
+
+// ============================================ REST PATTERN ===================================================
+
+// ============= 1. FOR DESTRUCTURING
+// SPREAD, because on the RIGHT side of "="
+const arr = [1, 2, ...[3, 4]];
+console.log(arr); // [1, 2, 3, 4]
+
+// REST, because on the LEFT side of "=" (rest = rest of the elements, i.e., should always be the last element)
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others); // 1 2 [3, 4, 5]
+
+// ===== Combining both by using the restaurant example from above
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood); // Pizza Risotto [ 'Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad' ]
+
+// ==== REST in Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays); // { thu: { open: 12, close: 22 }, fri: { open: 11, close: 23 } }
+
+// ========== 2. FOR FUNCTIONS
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log('Sum is', sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushroom', 'pineapple', 'olive', 'lettuce'); // Output:
+// mushroom
+// [ 'pineapple', 'olive', 'lettuce' ]
+
+restaurant.orderPizza('mushroom');
