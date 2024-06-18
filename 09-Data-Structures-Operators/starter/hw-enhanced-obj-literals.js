63077d3ac79366a31683c7e5b85a2515356bde08
idosumit <psumitcoding@gmail.com>
2024-06-18 11:23:16 +0900

    update: solved enhanced bj literals hw

---
+++ 09-Data-Structures-Operators/starter/hw-enhanced-obj-literals.js
@@ -0,0 +1,29 @@
+// questions from: https://codingheroes.io/assignments/enhanced-object-literals.html
+
+'use strict';
+
+// 9.1
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+
+console.log(newBook);
+
+// 9.2
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+};
+
+console.log(newBook2);
