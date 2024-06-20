5b1d409abfab9c8b4535bc068b1d08997bdca02d
idosumit <psumitcoding@gmail.com>
2024-06-20 10:27:34 +0900

    update: maps iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -696,8 +696,6 @@ console.log(
 
 console.log(new Set('charlesbarkely').size); // 10
 
-*/
-
 // ============================================ MAPS ===================================================
 
 const rest = new Map();
@@ -745,3 +743,58 @@ console.log(rest.get(arr)); // test
 
 //+++++++ DOM
 // rest.set(document.querySelector('h1'), 'Heading');
+
+*/
+
+// ============================================ MAPS: ITERATION ===================================================
+
+// ++++ there is another method for iterating maps other than .set
+
+const question = new Map([
+  ['question', 'what is the best prog language?'],
+  [1, 'C'],
+  [2, 'Python'],
+  [3, 'JS'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'try again'],
+]);
+
+console.log(question);
+
+// in our previous learnings, we had:
+const openingHours = {
+  mon: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+console.log(Object.entries(openingHours));
+
+//+++++++++++++ Converting this object to map
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//+++++++++++ MAPS ARE ITERABLE
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer')); // this doesn't work in node, browser is needed
+// console.log(answer);
+
+// console.log(question.get(question.get('correct') === answer));
+
+//+++++++++++ Converting map to array
+console.log([...question]);
+console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
