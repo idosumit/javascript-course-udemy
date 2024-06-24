444db6b467598f6b3926a3283001a16dfc79266c
idosumit <psumitcoding@gmail.com>
2024-06-24 09:36:21 +0900

    completed: data structures section

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -969,8 +969,6 @@ document.querySelector('button').addEventListener('click', function () {
   }
 });
 
-*/
-
 // ============================================ STRING METHODS EXTENDED ===================================================
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -988,3 +986,7 @@ for (const flight of flights.split('+')) {
   )})`.padStart(44, '.');
   console.log(output);
 }
+
+*/
+
+// ============================================ DATA STRUCTURES: FIN ===================================================
