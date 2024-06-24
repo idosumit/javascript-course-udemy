4eb084ad025d7fa755722944303807f185e01ed6
idosumit <psumitcoding@gmail.com>
2024-06-24 09:28:24 +0900

    update: completed extended string methods practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -947,8 +947,6 @@ const planesInLine = function (n) {
 
 planesInLine(5);
 
-*/
-
 // ============================================ CODING CHALLENGE #4 ===================================================
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
@@ -970,3 +968,23 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
+
+*/
+
+// ============================================ STRING METHODS EXTENDED ===================================================
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(44, '.');
+  console.log(output);
+}
