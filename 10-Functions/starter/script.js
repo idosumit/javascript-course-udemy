9dedab3318ba55853f0c0001d648e0ac23e56238
idosumit <psumitcoding@gmail.com>
2024-06-24 10:03:53 +0900

    update: passing arguments (value v reference)

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
+/*
+
 // ========================================== DEFAULT PARAMETERS =========================================
 const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+const createBoking = function (flightNum, numPassengers = 1, price = 199) {
   //   // old way (ES5) of setting default parameters
   //   numPassengers = numPassengers || 1;
   //   price = price || 199;
@@ -18,3 +20,41 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 };
 createBooking('LH123');
 createBooking('LH123', undefined, 100); // skipping a default parameter
+
+*/
+
+// ========================================== PASSING ARGUMENTS: VALUE V REFERENCE =========================================
+
+const flight = 'LH234';
+const charles = {
+  name: 'Charles Barkley',
+  passport: 2478042984,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 2478042984) {
+    alert('Check-in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, charles);
+// console.log(flight);
+// console.log(charles);
+
+// is the same as doing...
+// const flightNum = flight; // primitives won't change, their created copies (in this case flighNum) will
+// const passenger = charles; // objects can be changed
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random()) * 1000000000000000;
+};
+
+newPassport(charles);
+checkIn(flight, charles);
+
+// ++++++++ IMPORTANT: JS does not have pass by reference. ONLY pass values. Even the object above was not passing by reference, it was value.
