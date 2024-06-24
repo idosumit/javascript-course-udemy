0f305efe7b8a6c80067e0448464357439931ab56
idosumit <psumitcoding@gmail.com>
2024-06-24 09:46:16 +0900

    update: default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// ========================================== DEFAULT PARAMETERS =========================================
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  //   // old way (ES5) of setting default parameters
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+createBooking('LH123', undefined, 100); // skipping a default parameter
