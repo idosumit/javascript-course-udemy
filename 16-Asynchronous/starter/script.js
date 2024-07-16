040b256ce6cd1e7feb8748f1cb38edc35fb26d93
idosumit <psumitcoding@gmail.com>
2024-07-16 16:31:05 +0900

    important: first ajax call (XMLHttpRequest) of web api

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,58 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+{
+  /*
+The base URL of the API used throughout this section has changed
+
+It's not a big deal, it's really just one small change. Instead of:
+
+https://restcountries.eu/rest/v2/
+
+It's now:
+
+https://countries-api-836d.onrender.com/countries/
+*/
+}
+
+// ============================================ MAKING OUR FIRST API CALL ============================================
+
+//++++++++ XML HTTP request (old school way)
+const getCountryData = country => {
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://restcountries.com/v3.1/name/${country}?fullText=true`
+  );
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('japan');
+getCountryData('north korea');
+getCountryData('south korea');
