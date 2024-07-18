8a76d9dcfda113e50a72502c52373b23f046dcad
idosumit <psumitcoding@gmail.com>
2024-07-18 16:34:51 +0900

    callback hell demonstration

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -18,6 +18,8 @@ https://countries-api-836d.onrender.com/countries/
 */
 }
 
+/*
+
 // ============================================ MAKING OUR FIRST API CALL ============================================
 
 //++++++++ XML HTTP request (old school way)
@@ -56,6 +58,70 @@ const getCountryData = country => {
   });
 };
 
-getCountryData('japan');
-getCountryData('north korea');
-getCountryData('south korea');
+getCountryData('portugal');
+getCountryData('spain');
+
+*/
+
+// ============================================ CALLBACK HELL ============================================
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = country => {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://restcountries.com/v3.1/name/${country}?fullText=true`
+  );
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // rendering country 1
+    renderCountry(data);
+
+    // getting the neighbour country:
+    const neighbour = data.borders?.[0];
+    if (!neighbour) return;
+
+    // AJAX call for the neighbouring country
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('portugal');
+
+// But what if we needed the neighbour of the neighbour of the neighbour and so on? We would have to do asynchronous calls inside asynchronous calls inside asynchronous calls, and this is what we call callback hell. AJAX calls inside AJAX calls inside AJAX calls, and this is a real problem in JavaScript. It's a problem because it makes our code really hard to read and to maintain. And this is why we have promises and async/await. Will study this plus promises soon.
